b5bb33a10217dfff308940b4f05b6c4a0ffb6ea1
ikuli <boyindaspace@gmail.com>
2024-11-22 18:38:28 +0500

    wagtail create

---
+++ mysite/mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-&4!@(-4qd%oml0m4$ef480*7!p$9ov8#vjtr+f4t5w3wb=%b!h"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
