945b635250e137573f02dbb258316563765beafa
Treglazov Egor <etreglazov@datanomica.org>
2024-12-01 20:54:49 +0300

    fix moysklad

--- moysklad/entities/MoySkladApi.py
+++ moysklad/entities/MoySkladApi.py
@@ -24,7 +24,6 @@ class OrganizationClient(MoySkladBaseClient):
     def __init__(self, login, password, base_url, verify_ssl=True):
         super().__init__(login, password, base_url, "organization", verify_ssl)
 
-
 class CustomerOrderClient(MoySkladBaseClient):
     def __init__(self, login, password, base_url, verify_ssl=True):
         super().__init__(login, password, base_url, "customerorder", verify_ssl)
@@ -34,5 +33,70 @@ class CustomerOrderClient(MoySkladBaseClient):
             "organization": {"meta": {"href": organization}},
             "agent": {"meta": {"href": agent}},
             "positions": positions
+        }
+        return super().post(data)
+
+class CustomerClient(MoySkladBaseClient):
+    def __init__(self, login, password, base_url, verify_ssl=True):
+        super().__init__(login, password, base_url, "customer", verify_ssl)
+
+class ProductCategoryClient(MoySkladBaseClient):
+    def __init__(self, login, password, base_url, verify_ssl=True):
+        super().__init__(login, password, base_url, "productcategory", verify_ssl)
+
+class ShoppingCartClient(MoySkladBaseClient):
+    def __init__(self, login, password, base_url, verify_ssl=True):
+        super().__init__(login, password, base_url, "shoppingcart", verify_ssl)
+
+class OrderClient(MoySkladBaseClient):
+    def __init__(self, login, password, base_url, verify_ssl=True):
+        super().__init__(login, password, base_url, "order", verify_ssl)
+
+    def create_order(self, customer, status):
+        data = {
+            "customer": {"meta": {"href": customer}},
+            "status": status
+        }
+        return super().post(data)
+
+class PaymentMethodClient(MoySkladBaseClient):
+    def __init__(self, login, password, base_url, verify_ssl=True):
+        super().__init__(login, password, base_url, "paymentmethod", verify_ssl)
+
+    def create_payment_method(self, name):
+        data = {
+            "name": name
+        }
+        return super().post(data)
+
+class ShippingMethodClient(MoySkladBaseClient):
+    def __init__(self, login, password, base_url, verify_ssl=True):
+        super().__init__(login, password, base_url, "shippingmethod", verify_ssl)
+
+    def create_shipping_method(self, name):
+        data = {
+            "name": name
+        }
+        return super().post(data)
+
+class ReviewClient(MoySkladBaseClient):
+    def __init__(self, login, password, base_url, verify_ssl=True):
+        super().__init__(login, password, base_url, "review", verify_ssl)
+
+    def create_review(self, product, text):
+        data = {
+            "product": {"meta": {"href": product}},
+            "text": text
+        }
+        return super().post(data)
+
+class ShippingAddressClient(MoySkladBaseClient):
+    def __init__(self, login, password, base_url, verify_ssl=True):
+        super().__init__(login, password, base_url, "shippingaddress", verify_ssl)
+
+    def create_shipping_address(self, customer, address):
+        data = {
+            "customer": {"meta": {"href": customer}},
+            "address": address
         }
         return super().post(data)
