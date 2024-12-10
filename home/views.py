ec9398b09f2590d4b4867947e3a7a1015f44bbf7
Treglazov Egor <etreglazov@datanomica.org>
2024-12-10 11:46:49 +0300

    FIX html

--- home/views.py
+++ home/views.py
@@ -10,7 +10,7 @@ def index(request):
     return render(request, 'home/index.html')
 
 def company(request):
-    return render(request, 'home/page40704823body.html')
+    return render(request, 'home/page39291702body.html')
 
 def delivery(request):
     return render(request, 'home/delivery.html')
